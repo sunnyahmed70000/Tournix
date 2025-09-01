6d2283334b6d3b020ddf9c29f16afa25242d9fcd
sunnyahmed70000 <sunnyahmed70000@gmail.com>
2025-09-01 10:57:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// script.js
+
 console.log("PWA Website loaded!");
+
+// Service Worker Register
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => console.log("✅ Service Worker registered"))
+      .catch((err) => console.log("❌ Service Worker error:", err));
+  });
+}
