0b45725b05adecfef821faa9fb140ec1bbc21649
sunnyahmed70000 <sunnyahmed70000@gmail.com>
2025-09-01 11:00:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// script.js
-
-console.log("PWA Website loaded!");
-
-// Service Worker Register
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./sw.js")
-      .then(() => console.log("✅ Service Worker registered"))
-      .catch((err) => console.log("❌ Service Worker error:", err));
-  });
-}
+navigator.serviceWorker
+  .register("/Tournix/sw.js")
+  .then(() => console.log("✅ Service Worker registered"))
+  .catch((err) => console.log("❌ Service Worker error:", err));
