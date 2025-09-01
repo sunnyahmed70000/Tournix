0e8e4eb433b4df2f23f67714e65d8b82c9e41cbf
sunnyahmed70000 <sunnyahmed70000@gmail.com>
2025-09-01 11:06:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/Tournix/sw.js")
+      .register("/Tournix/sw.js")   // <-- তোমার repo নাম বসাও
       .then(() => console.log("✅ Service Worker registered!"))
       .catch((err) => console.log("❌ SW registration failed:", err));
   });
