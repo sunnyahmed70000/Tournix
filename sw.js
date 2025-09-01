58fa73ec4cccff53109b42781ec023b445d5b1c5
sunnyahmed70000 <sunnyahmed70000@gmail.com>
2025-09-01 10:53:12 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installed");
+  console.log("Service Worker installing...");
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated");
+  console.log("Service Worker activating...");
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  console.log("Fetching:", event.request.url);
+  event.respondWith(fetch(event.request));
 });
